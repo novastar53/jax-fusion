6484eb8657e46b97d2fcb868ddf7c34f3cf259aa
Vikram Pawar <pvikram035@gmail.com>
2025-09-28 06:27:29 -0400

    Fixed bug in vae impl.

--- src/jax_fusion/vae.py
+++ src/jax_fusion/vae.py
@@ -6,7 +6,7 @@ import flax.nnx as nnx
 
 import optax
 
-from jax_fusion.datasets.fashion_mnist import make_dataloader
+from jax_fusion.datasets.fashion_mnist import make_dataloader, visualize_batch, FASHION_LABELS
 
 train_it = make_dataloader("train")
 test_it = make_dataloader("test")
@@ -37,35 +37,37 @@ class VAE(nnx.Module):
         mu, log_var = jnp.split(x, 2, axis=1)
         key, subkey = jax.random.split(key)
         epsilon = jax.random.normal(subkey, log_var.shape)
-        l = mu + log_var # * epsilon
+        l = mu + log_var * epsilon
         x = self.linear2(l)
         x = x.reshape(B, 7, 7, 64)
         x = self.deconv1(x)
         y = self.deconv2(x)
         assert(batch.shape == y.shape)
+        y = y.transpose(0, 3, 1, 2)
         return y, key
 
 rngs = nnx.Rngs(default=0)
 m = VAE(Config(), rngs)    
 
-tx = optax.adam(1e-3)
+tx = optax.adam(1e-2)
 optimizer = nnx.Optimizer(m, tx, wrt=nnx.Param)
 
 
 def loss_fn(m, x, key):
     y, key = m(x, key)
-    loss =  jnp.sum((y - x) ** 2) / y.size
-    return loss, key
+    assert(y.shape == x.shape)
+    loss =  jnp.sum((y - x) ** 2) / y.shape[0]
+    return loss, (y, key)
 
 
 def step_fn(m, x, key):
-    (loss, key), grads = nnx.value_and_grad(loss_fn, has_aux=True)(m, x, key)
-    jax.debug.breakpoint()
+    (loss, (y, key)), grads = nnx.value_and_grad(loss_fn, has_aux=True)(m, x, key)
     optimizer.update(m, grads)
-    return loss, key
-
+    return loss, y, key
 
 key = jax.random.key(42)
-for x, _ in train_it:
-    loss, key = step_fn(m, x, key)
-    print(loss)
+for i, (x, labels) in enumerate(train_it):
+    loss, y, key = step_fn(m, x, key)
+    print(i, loss)
+
+visualize_batch(y, labels, FASHION_LABELS)
