853315c5c3b0786c18e92cb802e6e4dcdd62b827
Vikram Pawar <pvikram035@gmail.com>
2025-10-13 03:39:12 -0400

    Started working on unet module.

---
+++ src/jax_fusion/modules/unet.py
@@ -0,0 +1,12 @@
+import jax
+import jax.numpy as jnp
+import flax.nnx as nnx
+
+class UNet:
+    def __init__(self):
+        self.ups = []
+        self.downs = []
+    
+
+    def __call__(self, x):
+        pass
